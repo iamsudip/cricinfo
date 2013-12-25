6eda53ab0b40f9b455db44ce3544773125014b93
iamsudip <iamsudip@programmer.net>
2013-12-25 15:27:23 +0530

    Scraped scores and scheduled time

--- README.md
+++ README.md
@@ -7,12 +7,9 @@ Project outline:
 ----------------
 
 1. Scrap the live page to get the info for current international matches
-
-#. Now, Scrap
-
+2. Now, Scrap
 * matches results for the selected series
 * future schedule for the selected series
-
 3. Scrap scorecard page
-#. Ask user to save the data to a file or not
-#. continue
+4. Ask user to save the data to a file or not
+5. continue
--- cricinfo.py
+++ cricinfo.py
@@ -12,58 +12,63 @@ urilist = []
 espnurl = "http://www.espncricinfo.com"
 
 def prompt():
-	'''
-	Returns esponse from the user
-	'''
-	try:
-	    return int(raw_input("Select series: "))
-	except ValueError:
-		print "Seems you are good @English. Use the index number left side."
-		scrapresults(prompt())
+    '''
+    Returns esponse from the user
+    '''
+    try:
+        return int(raw_input("Select series: "))
+    except ValueError:
+        print "Seems you are good @English. Use the index number left side."
+        scrapresults(prompt())
 
 def scrapresults(choice):
-	try:
-		global htmlclient, urilist, espnurl
-		url = espnurl + urilist[choice-1].replace("content/current/", "engine/")
-		response = requests.get(url, headers=htmlclient)
+    try:
+        global htmlclient, urilist, espnurl
+        url = espnurl + urilist[choice-1].replace("content/current/", "engine/")
+        response = requests.get(url, headers=htmlclient)
 
-		# Scraping Results page for the selected series
-		if response.status_code==200:
-			soup = BeautifulSoup(response.text)
-			for matchestable in soup.findAll("div", attrs={"class":"div630Pad"}):
-				print matchestable
-
-	except IndexError:
-		print "Use the index number left side. Try again."
-		scrapresults(prompt())
+        # Scraping Results page for the selected series
+        if response.status_code==200:
+            soup = BeautifulSoup(response.text)
+            print soup.title.text + "\n"
+            for matchestable in soup.findAll("div", attrs={"class":"div630Pad"}):
+                scores = [ score.text for score in matchestable.findAll("p", attrs={"class":"potMatchText mat_scores"}) ]
+                status = [ status.text for status in matchestable.findAll("p", attrs={"class":"potMatchText mat_status"}) ]
+                for matches in matchestable.findAll("p", attrs={"class":"potMatchHeading"}):
+                    print matches.text
+                    print scores.pop(0)
+                    print status.pop(0) + "\n"
+        else:
+            print "Network Error. Try again later."
+    except IndexError:
+        print "Use the index number left side. Try again."
+        scrapresults(prompt())
 
 def main():
-	global htmlclient, urilist, espnurl
-	url = espnurl + "/ci/engine/match/scores/live.html"
-	response = requests.get(url, headers=htmlclient)
-	
-	# Scraping Live matches page and getting the info for current international series as a list
-	if response.status_code==200:
-	    soup = BeautifulSoup(response.text)
-	    for mainNav in soup.findAll("div", attrs={"id":"mainNav"}):
-	        for table in mainNav.findAll("table", attrs={"width":"270", "border":"0", "cellspacing":"0", "cellpadding":"0"}):
-	            urilist = [ uri.get("href") for uri in table.findAll("a") ]
-	else:
-		print "Network Error. Try again later."
+    global htmlclient, urilist, espnurl
+    url = espnurl + "/ci/engine/match/scores/live.html"
+    response = requests.get(url, headers=htmlclient)
+    
+    # Scraping Live matches page and getting the info for current international series as a list
+    if response.status_code==200:
+        soup = BeautifulSoup(response.text)
+        for mainNav in soup.findAll("div", attrs={"id":"mainNav"}):
+            for table in mainNav.findAll("table", attrs={"width":"270", "border":"0", "cellspacing":"0", "cellpadding":"0"}):
+                urilist = [ uri.get("href") for uri in table.findAll("a") ]
+    else:
+        print "Network Error. Try again later."
 
-	# Removing unnecessary 'ci' entry from list
-	del urilist[-1]
-	
-	# View the series to user for selection
-	for num, url in enumerate(urilist):
-	    print num+1, url.split("/")[1]
-	
-	scrapresults(prompt())
+    # Removing unnecessary 'ci' entry from list
+    del urilist[-1]
+    
+    # View the series to user for selection
+    for num, url in enumerate(urilist):
+        print num+1, url.split("/")[1]
+
+    scrapresults(prompt())
 
 if __name__ == '__main__':
-	try:
-		main()
-	except (KeyboardInterrupt, EOFError):
-		sys.exit(0)
-
-# http://www.espncricinfo.com/netstorage/648665.html
+    try:
+        main()
+    except (KeyboardInterrupt, EOFError):
+        sys.exit(0)
